0aea607824e8940dbc22f106e7362f742aa233ff
paulsasi <paulsasieta.upv@gmail.com>
2021-12-28 16:04:39 +0100

    Inventory rendering

--- frontend/munch_frontend/src/components/Inventory.js
+++ frontend/munch_frontend/src/components/Inventory.js
@@ -10,6 +10,8 @@ function Inventory(args) {
     const [inv, setInv] = useState([]);
     const updateInv = (value) => setInv(value);
 
+    const ITEMS_PER_LINE = 3;
+
 
     useEffect(() => {
         const fetchInventory = async () => {
@@ -20,23 +22,25 @@ function Inventory(args) {
         fetchInventory();
     }, [])
 
+    const renderInventory = inv.map((item, index) => {
 
-    console.log(inv);
-    console.log('PHIIIN');
-    console.log(inv[0]);
-    console.log('rassssh');
-    
+        const row = Math.floor(index / ITEMS_PER_LINE);
+        const col = index % ITEMS_PER_LINE;
 
+        /* const col = `${2 * 10}`; */
+        const formatted_row = `${1 + 6*row} / ${6 + 6*row}`;
+        const formatted_col = `${1 + 7*col} / ${8 + 7*col}`;
 
+        console.log({formatted_row});
+        console.log({formatted_col});
+        console.log('########################');
+        return (
+            <div className='inventory-item-box' style={{gridColumn: formatted_col, gridRow: formatted_row}}> {item.name} </div>
+        )
+    })
     return (
         <div className='inventory-wrapper'>
-            <div className='inventory-item-box' style={{gridColumn: '1 / 8', gridRow: '1 / 6'}}>haha</div>
-            <div className='inventory-item-box' style={{gridColumn: '8 / 15', gridRow: '1 / 6'}}>hola</div>
-            <div className='inventory-item-box' style={{gridColumn: '15 / 22', gridRow: '1 / 6'}}>hola</div>
-
-            <div className='inventory-item-box' style={{gridColumn: '1 / 8', gridRow: '6/ 11'}}>hola</div>
-            <div className='inventory-item-box' style={{gridColumn: '8 / 15', gridRow: '6 / 11'}}>hola</div>
-            <div className='inventory-item-box' style={{gridColumn: '15 / 22', gridRow: '6 / 11'}}>hola</div>
+            {renderInventory}
         </div>
     );
 }
