f7499387569fc5fbfb2457e0dca1b5dc78f5bc7e
paulsasi <paulsasieta.upv@gmail.com>
2021-12-29 16:31:17 +0100

    Image metadata render

--- frontend/munch_frontend/src/components/Beach.js
+++ frontend/munch_frontend/src/components/Beach.js
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import './Beach.css';
 import Inventory from './Inventory';
+import Footer from './Footer';
 
 function Beach() {
 
     const args = {"type" : "beach"};
+
     return (
         <div>
             <Header />
@@ -12,7 +14,13 @@ function Beach() {
                 <div className='currentsite'> &lt; Playa </div>
                 <h1 className='header'> Playa </h1>
                 <div className='inventory-container'><Inventory args={args} /></div>
+                <div className='beach-footer-container'>
+                    <Footer/>
+                </div>
             </div>
+
+            
+
             
         </div>
     );
--- frontend/munch_frontend/src/components/ItemInventory.js
+++ frontend/munch_frontend/src/components/ItemInventory.js
@@ -1,5 +1,6 @@
 import fetchImage from "../api/fetchImage";
 import './ItemInventory.css';
+import {BsCircleFill} from 'react-icons/bs';
 import Header from "./Header";
 import React, {useState, useEffect} from "react";
 
@@ -20,15 +21,23 @@ function ItemInventory(props) {
 
     }, [])
 
+    var h = 20;
+
+    const f = () => {
+        console.log('aaaaa');
+        h = 10;
+    }
+
     return (
         <div className='item-box-wrapper'>
             <div className='image-container'>
                 <img className='item-image'src={imageb64}></img>
             </div>
-            <div className='image-info'>hola</div>
+            <div className='image-name'>{item.name}</div>
+            <div className='image-price'>{item.price}€</div>
+            <BsCircleFill className='image-colors' color={'red'} onMouseEnter={f}> </BsCircleFill>{h}
         </div>
     );
 }
 
 export default ItemInventory;
-
